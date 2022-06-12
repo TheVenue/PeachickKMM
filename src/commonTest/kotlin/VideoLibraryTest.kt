94f74b0306942ed28922ac0efde99dd81a7c8854
marvinpatrick <marvindcpatrick@gmail.com>
2022-06-12 17:36:32 -0400

    Adding tests to ensure adding with no duplicates, and removing, currently at red in TDD.

---
+++ src/commonTest/kotlin/VideoLibraryTest.kt
@@ -0,0 +1,25 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class VideoLibraryTest {
+    @Test
+    fun addToMyStuffEnsureNoDuplicates() {
+        assertEquals(0, VideoLibrary.myStuff.size)
+        val video = VideoLibrary.featuredVideos[0]
+        VideoLibrary.addToMyStuff(video)
+        VideoLibrary.addToMyStuff(video)
+        VideoLibrary.addToMyStuff(video)
+        VideoLibrary.addToMyStuff(video)
+        assertEquals(1, VideoLibrary.myStuff.size)
+    }
+
+    @Test
+    fun removeFromMyStuff() {
+        assertEquals(0, VideoLibrary.myStuff.size)
+        val video = VideoLibrary.featuredVideos[0]
+        VideoLibrary.addToMyStuff(video)
+        assertEquals(1, VideoLibrary.myStuff.size)
+        VideoLibrary.removeFromMyStuff(video)
+        assertEquals(0, VideoLibrary.myStuff.size)
+    }
+}
